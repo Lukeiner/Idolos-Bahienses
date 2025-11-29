7687f2a293c4e0547ad154396b9b6ccb56c7c6f4
YACO99 <yyyyaaco@gmail.com>
2025-11-29 12:35:00 -0300

    Añadir Player

--- Codigos/Main.cs
+++ Codigos/Main.cs
@@ -2,37 +2,52 @@ using Godot;
 
 public partial class Main : Node
 {
+    public int Creditos = 5;
     PackedScene BasePlayer;
     Player P1,P2;
-    Node2D Map, Spawn;
+    Node2D Map, Spawn1, Spawn2;
 
     public override void _Ready()
     {
         var BasePlayer = ResourceLoader.Load<PackedScene>("res://Tscns/Player.tscn");
         Map = GetNode<Node2D>("Map");
-        Spawn = Map.GetNode<Node2D>("Spawn");
+        Spawn1 = Map.GetNode<Node2D>("Spawn1");
+        Spawn2 = Map.GetNode<Node2D>("Spawn2");
     }
 
     public override void _Process(double delta)
-	{
-		
-	}
+    {
+        if (Creditos > 0)
+        {
+            if (P1 == null & Input.IsActionJustPressed("Y"))
+            {
+                Spawn(1);
+                Creditos--;
+            }
 
-    public void Init(int i)
+            if (P2 == null & Input.IsActionJustPressed("3"))
+            {
+                Spawn(2);
+                Creditos--;
+            }
+        }
+    }
+
+    public void Spawn(int i)
     {
         switch (i) {
             case 1:
                 if (P1.IsInsideTree())
                     P1.QueueFree();
                 P1 = BasePlayer.Instantiate<Player>();
-                P1.numero = 1;
-                P1.Position = Spawn.Position;
+                P1.Init(1);
+                P1.Position = Spawn1.Position;
                 Map.AddChild(P1);
                 break;
             case 2:
                 P2 = BasePlayer.Instantiate<Player>();
-                P2.numero = 2;
-                P2.Position = Spawn.Position;
+                P2.Init(2);
+                P2.Position = Spawn2.Position;
                 Map.AddChild(P2);
                 break;
         }
--- Codigos/Player.cs
+++ Codigos/Player.cs
@@ -3,36 +3,35 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public int numero = 0;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
+	public void Init(int i)
+	{
+		if (i == 2)
+		{
+			//color rojo
+		}
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept"))
 		{
 			velocity.Y = JumpVelocity;
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
-		}
+            velocity.X = direction.X * Speed;
+            velocity.Y = direction.Y * Speed;
+        }
 		else
 		{
+
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 
 		Velocity = velocity;
