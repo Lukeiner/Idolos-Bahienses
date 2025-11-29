5fabf686f793db75a98df1a46c0f21041c73538f
YACO99 <yyyyaaco@gmail.com>
2025-11-29 14:28:59 -0300

    asd123

--- Codigos/Main.cs
+++ Codigos/Main.cs
@@ -2,37 +2,65 @@ using Godot;
 
 public partial class Main : Node
 {
+    public int Creditos = 5;
     PackedScene BasePlayer;
-    Player P1,P2;
-    Node2D Map, Spawn;
-
+    Player P1=null,P2=null;
+    Node2D Map, Spawn1, Spawn2;
+    Label CrediLabel;
+    Control ContrMain;
     public override void _Ready()
     {
         var BasePlayer = ResourceLoader.Load<PackedScene>("res://Tscns/Player.tscn");
         Map = GetNode<Node2D>("Map");
-        Spawn = Map.GetNode<Node2D>("Spawn");
+        Spawn1 = Map.GetNode<Node2D>("Spawn1");
+        Spawn2 = Map.GetNode<Node2D>("Spawn2");
+        ContrMain = GetNode<Control>("Control");
+        CrediLabel = ContrMain.GetNode<Label>("CrediLabel");
+        CrediLabel.Text = "Creditos: 5";
     }
 
     public override void _Process(double delta)
-	{
-		
-	}
-
-    public void Init(int i)
     {
+        if (Creditos > 0)
+        {
+            if (P1 == null & Input.IsActionJustPressed("Y"))
+            {
+                Creditos--;
+                Spawn(1);
+            }
+
+            if (P2 == null & Input.IsActionJustPressed("3"))
+            {
+                Creditos--;
+                Spawn(2);
+            }
+        }
+        else
+        {
+            //Fin del juego, musica mala
+            //10 segundos, pedir coins caso contraio reiniciar escena.
+        }
+    }
+
+    public void Spawn(int i)
+    {
+        CrediLabel.Text = "Creditos: " + Creditos;
+
         switch (i) {
             case 1:
-                if (P1.IsInsideTree())
+                if (P1!=null)
                     P1.QueueFree();
                 P1 = BasePlayer.Instantiate<Player>();
-                P1.numero = 1;
-                P1.Position = Spawn.Position;
+                P1.Init(1);//configura color y colicion
+                P1.Position = Spawn1.Position;
                 Map.AddChild(P1);
                 break;
             case 2:
+                if (P2 != null)
+                    P2.QueueFree();
                 P2 = BasePlayer.Instantiate<Player>();
-                P2.numero = 2;
-                P2.Position = Spawn.Position;
+                P2.Init(2);//configura color y colicion
+                P2.Position = Spawn2.Position;
                 Map.AddChild(P2);
                 break;
         }
--- Codigos/Player.cs
+++ Codigos/Player.cs
@@ -3,32 +3,8 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public int numero = 0;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-<<<<<<< Updated upstream
-
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-=======
 	public AnimationPlayer anim;
 	double FStop=0;
 	int t = 0;
@@ -53,16 +29,27 @@ public partial class Player : CharacterBody2D
 		Vector2 velocity = Velocity;
 		Vector2 direction = Input.GetVector(keys[1+t], keys[3+t], keys[0+t], keys[2+t]);
 		if (direction != Vector2.Zero && FStop == 0)
->>>>>>> Stashed changes
 		{
 			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+		}
+		else if(FStop == 0)
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+			FStop -= delta;
+			if (FStop < 0)
+				FStop = 0;
+        }
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	public void Damage()
+	{
+
 	}
 }
