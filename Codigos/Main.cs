dc7c37e43f070e7651b1dc6c65afe61615ff9a92
YACO99 <yyyyaaco@gmail.com>
2025-11-29 16:27:36 -0300

    arcade

--- Codigos/Main.cs
+++ Codigos/Main.cs
@@ -5,31 +5,72 @@ public partial class Main : Node
     public int Creditos = 5;
     PackedScene BasePlayer;
     Player P1=null,P2=null;
-    Node2D Map, Spawn1, Spawn2;
+    Node2D  Spawn1, Spawn2, Flecha;
     Label CrediLabel;
     Control ContrMain;
+    CollisionShape2D areaNext, areaNextEnd;
+    double TimeDelay = 0;
+    bool boMap = false;
+    Camera2D Cam;
+    
     public override void _Ready()
     {
-        var BasePlayer = ResourceLoader.Load<PackedScene>("res://Tscns/Player.tscn");
-        Map = GetNode<Node2D>("Map");
-        Spawn1 = Map.GetNode<Node2D>("Spawn1");
-        Spawn2 = Map.GetNode<Node2D>("Spawn2");
+        BasePlayer = ResourceLoader.Load<PackedScene>("res://Tscns/Player.tscn");
+        Cam = GetNode<Camera2D>("Cam");
+        Flecha = Cam.GetNode<Node2D>("Flecha");
+        Spawn1 = Cam.GetNode<Node2D>("Spawn1");
+        Spawn2 = Cam.GetNode<Node2D>("Spawn2");
         ContrMain = GetNode<Control>("Control");
         CrediLabel = ContrMain.GetNode<Label>("CrediLabel");
         CrediLabel.Text = "Creditos: 5";
+        areaNext = Cam.GetNode<CollisionShape2D>("Next/CollisionShape2D");
+        areaNextEnd = Cam.GetNode<CollisionShape2D>("End/CollisionShape2D");
     }
 
     public override void _Process(double delta)
     {
         if (Creditos > 0)
         {
-            if (P1 == null & Input.IsActionJustPressed("Y"))
+            if (P1 == null && Input.IsActionJustPressed("U") && !boMap)
             {
                 Creditos--;
                 Spawn(1);
             }
+            if (P1 != null && P2 != null)
+            {
+                if (P1.Position.Y < P2.Position.Y)
+                {
+                    P1.ZIndex = 10;
+                    P2.ZIndex = 20;
+                }
+                else if (P2.Position.Y < P1.Position.Y)
+                {
+                    P1.ZIndex = 20;
+                    P2.ZIndex = 10;
+                }
+            }
+            if (P1 != null || P2 != null)
+            {
+                if (boMap)
+                {
+                    Cam.Position += (float)delta*512*Vector2.Right;
+                }
+                if (TimeDelay > 0)
+                    TimeDelay -= delta;
+                if (TimeDelay < 0)
+                {
+                    //ver flecha
+                    Flecha.Visible = true;
+                    //enable colision de area al borde final
+                    areaNext.Disabled = false;
+                    TimeDelay = 0;
+                }
+                var en=GetTree().GetNodesInGroup("enemigo");
+                if (en.Count==0 && TimeDelay==0 && areaNext.Disabled)
+                    TimeDelay =4;
 
-            if (P2 == null & Input.IsActionJustPressed("3"))
+            }
+            if (P2 == null && Input.IsActionJustPressed("3") && !boMap)
             {
                 Creditos--;
                 Spawn(2);
@@ -52,17 +93,62 @@ public partial class Main : Node
                     P1.QueueFree();
                 P1 = BasePlayer.Instantiate<Player>();
                 P1.Init(1);//configura color y colicion
-                P1.Position = Spawn1.Position;
-                Map.AddChild(P1);
+                P1.GlobalPosition = Spawn1.GlobalPosition;
+                AddChild(P1);
                 break;
             case 2:
                 if (P2 != null)
                     P2.QueueFree();
                 P2 = BasePlayer.Instantiate<Player>();
                 P2.Init(2);//configura color y colicion
-                P2.Position = Spawn2.Position;
-                Map.AddChild(P2);
+                P2.GlobalPosition = Spawn2.GlobalPosition;
+                
+                AddChild(P2);
                 break;
         }
+    }
+
+    public void _on_end_body_entered(Node2D node)
+    {
+        if (node.IsInGroup("player") && ((Player)node).id==1)
+        {
+            areaNextEnd.CallDeferred(CollisionShape2D.MethodName.SetDisabled, true);
+            if (P1 != null)
+            {
+                P1.FStop = 0.2;
+                P1.anim.Play();
+
+            }
+            if (P2 != null)
+            {
+                P2.FStop = 0.2;
+                P2.anim.Play();
+            }
+            boMap = false;
+        }
+    }
+
+    public void _on_area_2d_body_entered(Node2D node)
+    {
+        if (node.IsInGroup("player") && ((Player)node).id == 1)
+        {
+            
+            Flecha.CallDeferred(Sprite2D.MethodName.SetVisible, false);
+            areaNext.CallDeferred(CollisionShape2D.MethodName.SetDisabled, true);
+            areaNextEnd.CallDeferred(CollisionShape2D.MethodName.SetDisabled, false);
+            if (P1 != null)
+            {
+                P1.FStop = 10;
+                P1.anim.Pause();
+            }
+            if (P2 != null)
+            {
+                P2.FStop = 10;
+                P2.anim.Pause();
+            }
+            boMap = true;
+
+        }
+
     }
 }
--- Codigos/Player.cs
+++ Codigos/Player.cs
@@ -6,40 +6,51 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	public AnimationPlayer anim;
-	double FStop=0;
+	Node2D cuerpo;
+	public double FStop=0;
 	int t = 0;
+	public int id = 0;
 	string[] keys= new string[] { "W","A","S","D","T","Y","U","Up", "Le", "Do", "Ri", "1", "2", "3" };
     public override void _Ready()
     {
 		anim = GetNode<AnimationPlayer>("Anim");
+        anim.Play("Start");
+		cuerpo = GetNode<Node2D>("Cuerpo");
     }
 	public void Init(int i)
 	{
-		
-		anim.Play("Start");
 		FStop = 0.3333;
 		if (i == 2)
 		{
+			id = 2;
 			t = 7;
 			CollisionLayer = 2;
+			CollisionMask = 2;
+		}
+		else
+		{
+			id = 1;
 		}
 	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-		Vector2 direction = Input.GetVector(keys[1+t], keys[3+t], keys[0+t], keys[2+t]);
+		Vector2 direction = Input.GetVector(keys[1 + t], keys[3 + t], keys[0 + t], keys[2 + t]);
 		if (direction != Vector2.Zero && FStop == 0)
 		{
-			velocity.X = direction.X * Speed;
+			cuerpo.Scale = Vector2.Down + ((direction.X < 0) ? Vector2.Left : Vector2.Right);
+			anim.Play("Correr");
+            velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
-		}
-		else if(FStop == 0)
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 		else
 		{
+            anim.Play("Quieto");
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+		if(FStop>0)
+        {
 			FStop -= delta;
 			if (FStop < 0)
 				FStop = 0;
@@ -50,6 +61,7 @@ public partial class Player : CharacterBody2D
 	}
 	public void Damage()
 	{
-
-	}
+        anim.Play("Daño");
+		FStop = 0.25;
+    }
 }
