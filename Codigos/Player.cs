ac209757564e62719b7b8d33094f79daffb6b613
YACO99 <yyyyaaco@gmail.com>
2025-11-29 14:27:45 -0300

    1

--- Codigos/Player.cs
+++ Codigos/Player.cs
@@ -6,6 +6,7 @@ public partial class Player : CharacterBody2D
 	public int numero = 0;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+<<<<<<< Updated upstream
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -27,6 +28,32 @@ public partial class Player : CharacterBody2D
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
+=======
+	public AnimationPlayer anim;
+	double FStop=0;
+	int t = 0;
+	string[] keys= new string[] { "W","A","S","D","T","Y","U","Up", "Le", "Do", "Ri", "1", "2", "3" };
+    public override void _Ready()
+    {
+		anim = GetNode<AnimationPlayer>("Anim");
+    }
+	public void Init(int i)
+	{
+		
+		anim.Play("Start");
+		FStop = 0.3333;
+		if (i == 2)
+		{
+			t = 7;
+			CollisionLayer = 2;
+		}
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		Vector2 direction = Input.GetVector(keys[1+t], keys[3+t], keys[0+t], keys[2+t]);
+		if (direction != Vector2.Zero && FStop == 0)
+>>>>>>> Stashed changes
 		{
 			velocity.X = direction.X * Speed;
 		}
